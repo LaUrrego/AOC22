dc8e5377d333967fee2dac051698d5a36d5ea7c1
LaUrrego <lurrego22@gmail.com>
2022-12-04 20:46:33 +0100

    Add day 4 submission

---
+++ day4/part1/day4_part1.py
@@ -0,0 +1,25 @@
+with open("day4/input/input.txt") as file:
+    total_match = 0
+    for line in file:
+        clean_line = line.strip()
+        elf1, elf2 = clean_line.split(",")
+        split = elf1.split("-")
+        elf1_range = [x for x in range(int(split[0]),int(split[1]) + 1)]
+        split = elf2.split("-")
+        elf2_range = [x for x in range(int(split[0]),int(split[1]) + 1)]
+        pair = [elf1_range, elf2_range]
+        sorted_pair = sorted(pair, key=len)
+        print(sorted(pair, key=len))
+        in_count = 0
+        match = 0
+        for item in sorted_pair[0]:
+            if item in sorted_pair[1]:
+                print("The matching numbers are:", item)
+                in_count += 1
+        print("count is:", in_count)
+        if len(sorted_pair[0]) == in_count:
+            match += 1
+        print("There was a total match:", match)
+        total_match += match
+        print("Match so far is:", total_match)
+print("Final count is:", total_match)
